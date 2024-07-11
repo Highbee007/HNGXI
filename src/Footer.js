b9ed5ea61309f2d706e3a08ba0da6f9bb90b7154
Highbee007 <ibrahimdamilola43@gmail.com>
2024-07-11 19:55:00 +0100

    deploy

--- src/Footer.js
+++ src/Footer.js
@@ -1,19 +1,20 @@
-import style from './Footer.module.css'
+import style from "./Footer.module.css";
 
 const Footer = () => {
-    return <div>
-        <div className={style.comment}>
-            <label>Comment here</label>
-            <input type="textarea" />
-            <button>Comment</button>
-        </div>
-        <div className={style.subscribe}>
-            <label>Subscribe for News and Updates</label>
-            <input type="email" />
-            <button>Subscribe</button>
-        </div>
+  return (
+    <div className={style.footer}>
+      <div className={style.comment}>
+        <label>Comment here</label>
+        <input type="textarea" />
+        <button>Comment</button>
+      </div>
+      <div className={style.subscribe}>
+        <label>Subscribe for News and Updates</label>
+        <input type="email" />
+        <button>Subscribe</button>
+      </div>
     </div>
+  );
 };
-
 
 export default Footer;
