ed16b48b1dfdb73da2ffcd6fd4554d44b9c0dd23
Highbee007 <ibrahimdamilola43@gmail.com>
2024-07-13 11:14:22 +0100

    deploy

--- src/Component/Checkout/Checkout.js
+++ src/Component/Checkout/Checkout.js
@@ -1,88 +1,90 @@
-import { Fragment, useState } from 'react';
-import img from './alpridephoto-9uxRcupG36I-unsplash.jpg';
-import style from './Checkout.module.css';
+import { Fragment, useState } from "react";
+import img from "./alpridephoto-9uxRcupG36I-unsplash.jpg";
+import style from "./Checkout.module.css";
+import { Link } from "react-router-dom";
 
 const Checkout = (props) => {
   const total = props.total();
 
-  const [shippingDetails, setShippingDetails] = useState({
-    street: '',
-    email: '',
-    phone: '',
+  const [cardDetails, setcardDetails] = useState({
+    cardNumber: "",
+    securityCode: "",
+    phone: "",
     saveDetails: false,
   });
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
-    setShippingDetails((prevDetails) => ({
+    setcardDetails((prevDetails) => ({
       ...prevDetails,
-      [name]: type === 'checkbox' ? checked : value,
+      [name]: type === "checkbox" ? checked : value,
     }));
   };
 
   return (
-     <Fragment> <h2>SHIPPING ADDRESS</h2>
-    <div className={style.checkout}>
-      <div className={style.formGroup}>
-        <div>
-        <div className={style.formControl}>
-          <label htmlFor="street">Street Address</label>
-          <input
-            type="text"
-            id="street"
-            name="street"
-            placeholder="Street Address"
-            value={shippingDetails.street}
-            onChange={handleChange}
-            aria-label="Street Address"
-          />
+    <Fragment>
+      <Link to="/HNGXI">
+        <button className={style.back}>{`<- Back to Shop`}</button>
+      </Link>
+      <h2>Pay With Credit Card</h2>
+      <div className={style.checkout}>
+        <div className={style.formGroup}>
+          <div>
+            <div className={style.card}>
+              <label for="cardNumber">Card Number</label>
+              <input
+                type="text"
+                id="cardNumber"
+                name="cardNumber"
+                placeholder="Card Number"
+                value={cardDetails.cardNumber}
+                onChange={handleChange}
+                aria-label="cardNumber"
+              />
+            </div>
+            <div className={style.others}>
+              <div className={style.control}>
+                <label for="expireDate">Expire Date</label>
+                <input
+                  type="date"
+                  id="expireDate"
+                  name="expireDate"
+                  value={cardDetails.expireDate}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className={style.control}>
+                <label for="securityCode">Security Code</label>
+                <input
+                  type="password"
+                  id="securityCode"
+                  name="securityCode"
+                  value={cardDetails.securityCode}
+                  onChange={handleChange}
+                  aria-label="Security Code"
+                />
+              </div>
+            </div>
+            <div className={style.pay}>
+              <input
+                type="checkbox"
+                id="saveDetails"
+                name="saveDetails"
+                checked={cardDetails.saveDetails}
+                onChange={handleChange}
+              />
+              <label for="Details">
+                Save my card details for Future Purchase
+              </label>
+              <button className={style.button} onClick={() => props.checkout()}>
+                PAY #{total.total}
+              </button>
+            </div>
+          </div>
         </div>
-        <div className={style.formControl}>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Enter Your Email eg.example@gmail.com"
-            value={shippingDetails.email}
-            onChange={handleChange}
-            aria-label="Email"
-          />
-        </div>
-        <div className={style.formControl}>
-          <label htmlFor="phone">Phone number</label>
-          <input
-            type="number"
-            id="phone"
-            name="phone"
-            placeholder="Input your Phone number"
-            value={shippingDetails.phone}
-            onChange={handleChange}
-            aria-label="Phone number"
-          />
-        </div>
-        <div className={style.formControl}>
-          <input
-            type="checkbox"
-            id="saveDetails"
-            name="saveDetails"
-            checked={shippingDetails.saveDetails}
-            onChange={handleChange}
-          />
-          <label for="saveDetails">Save my Shipping details</label>
-        </div></div>
-        <div>
-        <button
-          className={style.button}
-          onClick={() => props.checkout()}
-          aria-label={`Pay ${total.total}`}
-        >
-          PAY #{total.total.toFixed(2)}
-        </button>
+        <img src={img} alt="payment" className={style.paymentImg} />
       </div>
-      </div>
-      <img src={img} alt="payment" className={style.paymentImg} />
-    </div></Fragment>
+    </Fragment>
   );
 };
 
